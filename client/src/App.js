ba72c93416524e2df7a570a1608fba5d5598f61f
Nikita Zhiznevskiy <nzhigulevskiy@bk.ru>
2023-02-23 11:53:15 +0300

    mobx

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import Header from './Components/header/header';
 import Body from './Components/body';
-import { Route, BrowserRouter, Routes } from 'react-router-dom'
+import { Route, BrowserRouter, Routes,Navigate } from 'react-router-dom'
 import About from './Components/about';
 import Dialogs from './Components/Dialogs/Dialogs';
 import License from './Components/license';
@@ -10,12 +10,15 @@ import Register from './Components/auth/register';
 
 
 const App = (props) => {
+  const isAuth=false;
   return (
     <BrowserRouter>
       <div>
         <Header />
         <Routes>
+          {isAuth &&
             <Route path="/about" element={<About/>}/>
+          }
             <Route path="/dialogs/*" element={<Dialogs store={props.store} sendMessage={props.sendMessage}/>}/>
             <Route path="/weather" element={<Body weatherMethod={props.gettingWeather} state={props.state}/>}/>
             <Route path="/license" element={<License store={store} />}/>
